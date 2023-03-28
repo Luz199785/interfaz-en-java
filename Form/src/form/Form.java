f3d39bf19fcd9cfe5c395ea7230e089c3811e23e
Luz199785 <111939240+Luz199785@users.noreply.github.com>
2023-03-27 19:24:20 -0500

    Add files via upload

---
+++ Form/src/form/Form.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package form;
+
+/**
+ *
+ * @author 305
+ */
+public class Form {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       new frm1_formulario().setVisible(true);
+    }
+    
+}
